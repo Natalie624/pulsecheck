88a4ae09c8389896e0d32a861db705baf8e9f0c1
Natalie Cervantes <natalie.cervantes@gmail.com>
2025-07-02 14:21:57 -0700

    added clerk UI components and middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import "./styles/globals.css";
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">🚀 PulseCheck is Alive!</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <SignedIn>
+        <p className="text-lg">You&apos;re signed in!</p>
+        <UserButton />
+      </SignedIn>
+
+      <SignedOut>
+        <p className="text-lg">Please sign in:</p>
+        <SignInButton />
+      </SignedOut>
+     {/* <h1 className="text-3xl font-bold">🚀 PulseCheck is Alive!</h1> */}
     </main>
   );
 }
