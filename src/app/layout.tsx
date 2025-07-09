f24add25e9ca7601c3199596effc1e593ba76212
Natalie Cervantes <natalie.cervantes@gmail.com>
2025-07-09 12:19:46 -0700

    updated homepage look and feel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { ClerkProvider,
-    SignInButton,
-    SignUpButton,
     SignedIn,
-    SignedOut,
     UserButton,
  } from "@clerk/nextjs";
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -24,24 +21,15 @@ export default function RootLayout({
   <ClerkProvider>
     <html lang="en">
       <body>
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedIn>
+          <SignedIn>
+            {/* Header only whosn when signed in */}
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
               <button className="text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                 You&apos;re Signed In!
               </button>
               <UserButton />
+              </header>
             </SignedIn>
-
-            <SignedOut>
-              <SignInButton mode="modal" />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            
-          </header>
         {children}
         <SpeedInsights />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
-import "./styles/globals.css";
+'use client';
+
+import { SignInButton, SignUpButton, SignedOut } from '@clerk/nextjs';
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen"> 
-     <h1 className="text-3xl font-bold">Welcome to Pulse Check</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen px-6 py-16 bg-white text-center"> 
+     <h1 className="text-5xl md:text-6xl font-semibold leading-snug gradient">
+        Welcome to Pulse Check
+      </h1>
+
+      <p className="mt-4 max-w-2x1 text-lg text-gray-700">
+        An AI-powered status report generator that helps teams quickly summarize progress, blockers, and next steps from simple input prompts.
+      </p>
+
+      <div className="mt-8 flex gap-4">
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="bg-gray-200 text-gray-800 hover:bg-gray-300 rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-6 cursor-pointer">
+              Sign In
+            </button>
+          </SignInButton>
+
+          <SignUpButton mode="modal">
+            <button className="bg-[#6c47ff] text-white hover:bg-[#5a38e0] rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-6 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </SignedOut>
+      </div>
+ 
     </main>
   );
 }
