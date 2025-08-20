e157cad4bb4a1317f6883813baf7664032d54afa
Natalie Cervantes <natalie.cervantes@gmail.com>
2025-08-20 13:17:19 -0700

    added terms of service and privacy policy to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import GuardedRoutes from './components/GuardedRoutes';
 import AuthButtons from './components/AuthButtons';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function Home() {
@@ -29,7 +30,29 @@ export default function Home() {
       <div className="mt-8 flex gap-4">
         <AuthButtons />
       </div>
- 
+      
+      {/* Footer links */}
+      <footer className="mt-18 text-sm text-gray-400">
+        <div className="fflex flex-row items-center justify-center gap-6">
+              <Link
+                href="https://nataliecervantes.com/terms-of-service-pulsecheck"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline hover:text-gray-800 px-2"
+              >
+                Terms of Service
+              </Link>
+              <span>•</span>
+              <Link
+                href="https://nataliecervantes.com/privacy-policy-pulsecheck"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline hover:text-gray-800 px-2"
+              >
+                Privacy Policy
+              </Link>
+            </div>
+        </footer>
     </main>
     </GuardedRoutes>
     </>
